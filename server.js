dd6a7fad3742aaa9a377382eb75f637b23b13a95
Rjcode <jdrandy24@gmail.com>
2024-11-30 12:04:43 -0500

    setup exp app

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const express = require('express')
 
+const app = express()
+
+
+//turninary expression - if the port is avalible return that , other wise return 3000
+const PORT = process.env.PORT ? process.env.port : '3000';
+
+
+// root route
+app.get('/', (req, res) => {
+    res.send('Hello world')
+
+})
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`app started on ${PORT}`)
+})
